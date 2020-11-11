fae7aa40a1a928c87a8b3505afb695eacf30940f
Hedge-Hodge <oisinhodgins12@gmail.com>
2020-11-11 11:49:10 +0000

    scaffolded cart, declared concern

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,14 +1,15 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
-  end
-  def index_time
     @time = Time.now.strftime("%H:%M:%S ")
-    logger.debug "!!!!!!!!!!!"
-    logger.debug @time
   end
+  #def index_time
+  #  logger.debug "!!!!!!!!!!!"
+  #  logger.debug @time
+  #end
   def get_time
     @time = Time.now.strftime("%H:%M:%S ")
     render "store/date"
+    #render partial: "store/date"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :carts
   root to: 'store#index', as: 'store'
   resources :products
-  root "store#index_time"
-  get 'store/index_time'
+  #root "store#index_time"
+  #get 'store/index'
   get 'store/get_time'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
