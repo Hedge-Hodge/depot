1dd4a2cca6defca77dd56a11ff1bf744fa102d7d
Hedge-Hodge <oisinhodgins12@gmail.com>
2020-11-18 14:04:43 +0000

    include cart in store layout

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,7 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        #format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
         #format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
       else
