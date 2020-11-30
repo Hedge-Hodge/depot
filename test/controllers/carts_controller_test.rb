ae585482ccce6908c4b723ba8d387e8c884ba7fe
Hedge-Hodge <oisinhodgins12@gmail.com>
2020-11-30 17:41:00 +0000

    added checkout button

--- test/controllers/carts_controller_test.rb
+++ test/controllers/carts_controller_test.rb
@@ -39,6 +39,7 @@ class CartsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy cart" do
+    post line_items_url, params: { product_id: products(:ruby).id }
     @cart = Cart.find(session[:cart_id])
     assert_difference('Cart.count', -1) do
       session[:cart_id] = @cart.id
