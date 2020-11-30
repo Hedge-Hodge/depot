ca573cc0f600f7383913dfb2ce23857ff9e2cf52
Hedge-Hodge <oisinhodgins12@gmail.com>
2020-11-30 22:02:58 +0000

    added atom feed

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   root to: 'store#index', as: 'store'
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
   #root "store#index_time"
   #get 'store/index'
   get 'store/get_time'
